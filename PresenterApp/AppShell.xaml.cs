c4365aaab156719d5af078a1a18768996aa86a58
Việt <acdoc123@gmail.com>
2025-11-10 14:21:19 +0700

    add icon lib

--- PresenterApp/AppShell.xaml.cs
+++ PresenterApp/AppShell.xaml.cs
@@ -8,7 +8,6 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
-        InitializeComponent();
         var currentTheme = Application.Current!.RequestedTheme;
         ThemeSegmentedControl.SelectedIndex = currentTheme == AppTheme.Light ? 0 : 1;
 
